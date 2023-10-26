94c4988cbd9b26a2763565cb1bfe9f24d6981f72
yonatankhunters <yonatan.khanashvili@hunters.ai>
2023-10-26 11:41:28 +0300

    - add remote keys removal

--- src/private_key_creator.py
+++ src/private_key_creator.py
@@ -31,3 +31,33 @@ class PrivateKeyCreator:
             json.dump(key_data, file)  # Save the decoded key data, not the entire key object
 
         print(f"\033[92m \tKey created and saved to {file_path} \033[0m")
+
+    def delete_remote_key(self, key_name):
+        """ Delete the remote service account key """
+        try:
+            self.iam_service.projects().serviceAccounts().keys().delete(name=key_name).execute()
+            print(f"[!] Successfully deleted remote service account key: {key_name}")
+        except Exception as e:
+            print(f"Error deleting remote key {key_name}: {e}")
+
+    def delete_keys_without_dwd(self, confirmed_dwd_keys):
+        """ Delete SA keys which found without DWD from local folder and remotely"""
+        print("[!] Clearing private keys without DWD enabled ...")
+        for key_path in os.listdir(self.keys_directory):
+            full_path = os.path.join(self.keys_directory, key_path)
+            if full_path not in confirmed_dwd_keys:
+                try:
+                    # Delete the key remotely
+                    with open(full_path, 'r') as key_file:
+                        key_data = json.load(key_file)
+                        client_email = key_data["client_email"]
+                        key_id = key_data["private_key_id"]
+                        project_id = key_data["project_id"]
+                        # API is expecting the following format projects/{PROJECT_ID}/serviceAccounts/{SERVICE_ACCOUNT_EMAIL}/keys/{KEY_ID}
+                        resource_name = f"projects/{project_id}/serviceAccounts/{client_email}/keys/{key_id}"
+                        self.delete_remote_key(resource_name)
+                    # Delete the key locally
+                    os.remove(full_path)
+                    print(f"[!] Deleted local service account key without DWD: {full_path}")
+                except OSError as e:
+                    print(f"Error deleting {full_path}: {e}")
